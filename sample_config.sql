391cc58727f6dc153fac83d6982a99bc6140b8f0
alexmateescu <alex.mateescu34@gmail.com>
2019-06-30 08:52:23 +0100

    Update sample_config.sql

--- sample_config.sql
+++ sample_config.sql
@@ -1,5 +1,5 @@
-UPDATE pool.config SET item_value = 'ccx7VCqxxAQRT8CAXDZG7DAFj18wqsHWr2eaHDPRCR7YF3PqKB8oQC5FELavrewjMfZAcXU86zfuxCf1ZwvqKwcq65tHaRrSHg' WHERE module = 'pool' and item = 'address';
-UPDATE pool.config SET item_value = 'ccx7MuksPfTeSiRNRAkxsXZ5K4i5Xkn36hZpT28YSm3gFxqe9mU6Bc9LBDb1mRwnrcT8X5zcWByF5aWzuU2f2ox86PxyrM7haX' WHERE module = 'payout' and item = 'feeAddress';
+UPDATE pool.config SET item_value = '' WHERE module = 'pool' and item = 'address';
+UPDATE pool.config SET item_value = '' WHERE module = 'payout' and item = 'feeAddress';
 UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'mailgunKey';
 UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'mailgunURL';
 UPDATE pool.config SET item_value = '' WHERE module = 'general' and item = 'emailFrom';
